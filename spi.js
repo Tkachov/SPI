c3c7fbba4bb176f7ed2735af123f3377a8e59b00
Hunter <HunterNL@users.noreply.github.com>
2015-03-30 17:59:07 +0200

    Updated Meteor, got rid of some old code, added message when track has no meta-data

--- spi.js
+++ spi.js
@@ -8,29 +8,48 @@ if (Meteor.isClient) {
 		}
 	})
 
+	//Preload images, doesn't seem to work :s
+	Template.body.created = function(){
+		var r = [];
+		var i = 0;
+		["play.png","pause.png","next.png","previous.png","mute.png","unmute.png"].forEach(function(url){
+			r[i] = new Image();
+			r[i].src = url;
+			i++
+		})
+	}
+
+
+
 	Template.song.helpers({
 		slidervalue : function() {
-			return this.position/this.duration
+			//console.log(this.position,Date.now()-Template.parentData(1).lastupdatetime)
+			//return (this.position+Date.now()-Template.parentData(1).lastupdatetime)/this.duration
+			return (this.position/this.duration)
 		},
-		
+
 		playing : function() {
 			return this.state=="PLAYING"
 		},
-		
+
 		muted : function() {
 			return this.mute == 1
+		},
+
+		has_data : function() {
+			return !!(this.album||this.title||this.artist)
 		}
 	})
-	
+
 	Template.song.events({
 		"click button" : function(e,tmp) {
 			Meteor.call(e.target.name,tmp.data._id)
 		},
-		
+
 		"click img" : function(e,tmp) {
 			Meteor.call(e.target.dataset.cmd,tmp.data._id)
 		},
-		
+
 		"change input[type='range']" : function(e,tmp) {
 			console.log(tmp)
 			Meteor.call("pos",[tmp.data._id,e.target.value])
@@ -40,49 +59,44 @@ if (Meteor.isClient) {
 
 if (Meteor.isServer) {
 	Meteor.startup(function () {
-		
-		Sources.update({active:null},{$set : {active:false}},{multi: true})
+
+		function addSource(url,name) {
+			Sources.insert({
+				active: false,
+				url: url,
+				title: name,
+
+			})
+		}
+
+		Sources.remove({})
+		//Add sources here!
+		//addSource("192.168.1.1:7777","Foobar's desktop")
+
 		Meteor.setTimeout(mainloop,1000)
 		Meteor.setTimeout(idleloop,200)
 	});
-	  
+
 	function idleloop() {
 		Sources.find({active:false}).forEach(function(source){
 			updateSource(source)
 		})
-		
+
 		Meteor.setTimeout(idleloop,10000)
 	}
-	
+
 	function mainloop() {
 		Sources.find({active:true}).forEach(function(source){
 			updateSource(source)
 		})
-		
+
 		Meteor.setTimeout(mainloop,200)
 	}
-	  
+
 	function completeURL(url,https,command,value) {
 		return "http"+(https?"s":"")+"://"+url+"/API/1/"+(command?command.toUpperCase():"")+(value?"/"+value:"")
 	}
-	/*
-	function initupdateloop() {
-		//Update data
-		Sources.find().forEach(function(source){
-			if(source.url) {
-				updateSource(source)
-			} else {
-				console.log((source.title||"Unnamed source")+ " has no url")
-			}
-		})
-	}
-	
-	function scheduleUpdate(source,nextupdate) {
-		Meteor.setTimeout(function() {
-			updateSource(Sources.findOne(source._id))
-		},nextupdate-Date.now())
-	}*/
-	
+
 	function updateSource(source) {
 		HTTP.get(completeURL(source.url),undefined,function(err,res) {
 			if(err) {
@@ -95,33 +109,29 @@ if (Meteor.isServer) {
 			} else {
 				var rawdata = res.content.split("\r\n")
 				var data = {}
-				
-				var nextupdate = Date.now()+(source.updateinterval||1000)
-				
+
+
 				rawdata.forEach(function(entry){
 					var split = entry.split("=")
 					if(split[0]!="")
 					data[split[0].toLowerCase()]=split[1]
-				}) 
-				
+				})
+
 				Sources.update(source._id,{
 					$set : {
 						music_data : data,
 						active: true,
-						lastupdatetime : Date.now()
 					}
 				})
 			}
-			//scheduleUpdate(source,nextupdate)
-		}) 
+		})
 	}
-	
-	
+
+
 	function sendCommand(source,command,value) {
-		console.log(completeURL(source.url,false,command,Math.floor(value)))
 		HTTP.get(completeURL(source.url,false,command,Math.floor(value)))
 	}
-	
+
 	Meteor.methods({
 		play : function(sourceID) {sendCommand(Sources.findOne(sourceID),"play")},
 		pause : function(sourceID) {sendCommand(Sources.findOne(sourceID),"pause")},
@@ -132,9 +142,7 @@ if (Meteor.isServer) {
 		repeat : function(sourceID) {sendCommand(Sources.findOne(sourceID),"repeat")},
 		mute : function(sourceID) {sendCommand(Sources.findOne(sourceID),"mute")},
 		pos : function(args) {
-			console.log(args)
 			var source = Sources.findOne(args[0])
-			
 			sendCommand(source,"position",args[1]*source.music_data.duration)
 		}
 	})
